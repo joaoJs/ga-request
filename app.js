fe184f867c952e09307a1f702fd45760ba40c908
joaoJs <=>
2021-02-22 13:25:59 -0500

    adds cors

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
-const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
+// const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(cors(corsOptions));
+
 
 app.use(logger('dev'));
 app.use(express.json());
