1098b77770595fcb9b09ad8215522e677e2967d3
joaoJs <=>
2021-02-22 10:15:22 -0500

    checks private key

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,9 @@ const jwt = new google.auth.JWT(process.env.CLIENT_EMAIL, null, process.env.PRIV
 /* GET home page. */
 router.get('/', async function(req, res, next) {
 
+  console.log("++++++++++==========++++++++++")
+  console.log(process.env.PRIVATE_KEY);
+
   const defaults = {
     'auth': jwt,
     'ids': 'ga:' + process.env.VIEW_ID,
