3c24c2747ccf5327e8d1f9366f38bc08c1a9a10f
joaoJs <=>
2021-02-23 06:11:41 -0500

    adds real time api

--- routes/index.js
+++ routes/index.js
@@ -18,14 +18,18 @@ router.get('/', async function(req, res, next) {
     'ids': 'ga:' + process.env.VIEW_ID,
   }
   const response = await jwt.authorize().catch(err => console.log(err))
-  const result = await google.analytics('v3').data.ga.get({
+  const result = await google.analytics('v3').data.realtime.get({
     ...defaults,
     'start-date': '30daysAgo',
     'end-date': 'today',
-    'metrics': 'ga:sessions',
+    'metrics': 'rt:activeUsers',
   })
   //console.log(result)
-  res.send(result.data.rows[0][0])
+  if (result.data.rows) {
+    res.send(result.data.rows[0][0])
+  } else {
+    res.send('0');
+  }
   // console.dir(result.data.rows.sort((a, b) => b[1] - a[1]))
 });
 
@@ -44,7 +48,56 @@ router.get('/browser', async function(req, res, next) {
     'metrics': 'ga:sessions'
   })
   //console.log(result);
-  res.send(result.data.rows.sort((a, b) => b[1] - a[1]))
+  if (result.data.rows) {
+    res.send(result.data.rows.sort((a, b) => b[1] - a[1]))
+  } else {
+    res.send([['no', 'views']])
+  }
+});
+
+router.get('/blue', async function(req, res, next) {
+  const defaults = {
+    'auth': jwt,
+    'ids': 'ga:' + process.env.VIEW_ID_BLUE,
+  }
+  const response = await jwt.authorize().catch(err => console.log(err))
+  const result = await google.analytics('v3').data.realtime.get({
+    ...defaults,
+    'start-date': '30daysAgo',
+    'end-date': 'today',
+    'metrics': 'rt:activeUsers',
+  })
+  // console.log(result)
+  if (result.data.rows) {
+    res.send(result.data.rows[0][0])
+  } else {
+    res.send('0');
+  }
+  
+  // console.dir(result.data.rows.sort((a, b) => b[1] - a[1]))
+});
+
+router.get('/blue/browser', async function(req, res, next) {
+
+  const defaults = {
+    'auth': jwt,
+    'ids': 'ga:' + process.env.VIEW_ID_BLUE,
+  }
+  const response = await jwt.authorize().catch(err => console.log(err))
+  const result = await google.analytics('v3').data.ga.get({
+    ...defaults,
+    'start-date': '30daysAgo',
+    'end-date': 'today',
+    'dimensions': 'ga:browser',
+    'metrics': 'ga:sessions'
+  })
+  //console.log(result);
+  if (result.data.rows) {
+    res.send(result.data.rows.sort((a, b) => b[1] - a[1]))
+  } else {
+    res.send([['no', 'views']])
+  }
+
 });
 
 module.exports = router;
